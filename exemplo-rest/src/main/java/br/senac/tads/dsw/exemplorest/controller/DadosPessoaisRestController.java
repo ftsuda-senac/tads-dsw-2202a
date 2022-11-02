728c999b09137de36ab60804ea277c54760061b3
ftsuda-senac <ftsuda.senac@gmail.com>
2022-11-01 22:44:51 -0300

    CORS e salvar

--- exemplo-rest/src/main/java/br/senac/tads/dsw/exemplorest/controller/DadosPessoaisRestController.java
+++ exemplo-rest/src/main/java/br/senac/tads/dsw/exemplorest/controller/DadosPessoaisRestController.java
@@ -3,7 +3,6 @@ package br.senac.tads.dsw.exemplorest.controller;
 import br.senac.tads.dsw.exemplorest.dominio.DadosPessoais;
 import br.senac.tads.dsw.exemplorest.dominio.DadosPessoaisService;
 import java.net.URI;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import javax.validation.Valid;
@@ -12,6 +11,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,12 +20,13 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.HttpStatusCodeException;
 import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 @RequestMapping("/rest/pessoas")
+// @CrossOrigin(origins = { "http://localhost:8080", "http://10.35.27.175:8080" })
+@CrossOrigin(origins = "*")
 public class DadosPessoaisRestController {
     
     @Autowired
