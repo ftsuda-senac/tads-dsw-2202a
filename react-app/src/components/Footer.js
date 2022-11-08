abecd13613bcccf3498c20798eb39759e1e0bd74
ftsuda-senac <ftsuda.senac@gmail.com>
2022-11-08 20:45:05 -0300

    Implementação básica de alguns componentes

---
+++ react-app/src/components/Footer.js
@@ -0,0 +1,12 @@
+import './footer.css';
+
+export default function Footer () {
+
+    return (
+        <footer className="footer mt-auto py-3 bg-light">
+            <div className="container-lg">
+                <p className="text-muted text-center"><a href="https://github.com/ftsuda-senac" target="_blank" rel="nofollow noreferrer">@ftsuda-senac</a></p>
+            </div>
+        </footer>
+    );
+}
